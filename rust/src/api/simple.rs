bf62ee3c36aa8e23e6297db3a4cf1b13b4c20ca8
nir <nrbnlulu@gmail.com>
2025-01-13 10:35:00 +0200

    wip render with gl

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,13 +1,19 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}, thread};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 use flume::{bounded, Receiver, Sender};
 use irondash_engine_context::EngineContext;
 use irondash_run_loop::RunLoop;
-use irondash_texture::{BoxedPixelData, PayloadProvider, SendableTexture, SimplePixelData, Texture};
+use irondash_texture::{
+    BoxedPixelData, PayloadProvider, SendableTexture, SimplePixelData, Texture,
+};
 use log::{error, info};
 use simple_logger::SimpleLogger;
 
-use crate::core::{get_images, PixelBufferSource};
+use crate::core::{get_images, GlSource};
 
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn greet(name: String) -> String {
@@ -22,7 +28,6 @@ pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
 }
 
-
 lazy_static::lazy_static! {
 
     static ref TEXTURES_REGISTRY: Mutex<HashMap<i64, Arc<SendableTexture<BoxedPixelData>>>> = Mutex::new(HashMap::new());
@@ -32,26 +37,29 @@ pub fn create_that_texture_please(engine_handle: i64) -> anyhow::Result<i64> {
     let (tx_texture_id, rx_texture_id) = bounded(1);
 
     RunLoop::sender_for_main_thread().unwrap().send(move || {
-        if let Err(e) = EngineContext::get(){
+        if let Err(e) = EngineContext::get() {
             error!("Failed to get engine handle: {:?}", e);
         }
-    let (tx, rx) = bounded(2);
-    let provider = Arc::new(PixelBufferSource { rx });
+        let (tx, rx) = bounded(2);
+        let provider = Arc::new(GlSource { rx });
 
-    let texture = Texture::new_with_provider(engine_handle, provider).unwrap();
-    let texture_id = texture.id();
-    tx_texture_id.send(texture_id).expect("Failed to send texture");
+        let texture = Texture::new_with_provider(engine_handle, provider).unwrap();
+        let texture_id = texture.id();
+        tx_texture_id
+            .send(texture_id)
+            .expect("Failed to send texture");
 
-    let texture_arc = texture.into_sendable_texture();
-    {
-        let _ = TEXTURES_REGISTRY.lock().unwrap().insert(texture_id, texture_arc.clone());
+        let texture_arc = texture.into_sendable_texture();
+        {
+            let _ = TEXTURES_REGISTRY
+                .lock()
+                .unwrap()
+                .insert(texture_id, texture_arc.clone());
+        }
 
-    }
-
-    std::thread::spawn(move || {
-        get_images(tx, texture_arc).expect("Failed to get images");
-    });
-
+        std::thread::spawn(move || {
+            get_images(tx, texture_arc).expect("Failed to get images");
+        });
     });
     Ok(rx_texture_id.recv().unwrap())
 }
--- rust/src/core/mod.rs
+++ rust/src/core/mod.rs
@@ -1,48 +1,54 @@
 use std::{fs::File, sync::Arc, thread};
-
+mod gl;
 use flume::{bounded, Receiver, Sender};
 use flutter_rust_bridge::BaseAsyncRuntime;
 use image::{ImageBuffer, ImageReader, RgbaImage};
-use irondash_texture::{BoxedPixelData, PayloadProvider, SendableTexture, SimplePixelData, Texture};
+use irondash_texture::{
+    BoxedPixelData, PayloadProvider, SendableTexture, SimplePixelData, Texture,
+};
 use log::info;
 
 use crate::frb_generated::FLUTTER_RUST_BRIDGE_HANDLER;
 
 type Frame = (Vec<u8>, u32, u32);
 
-pub struct PixelBufferSource {
+pub struct GlSource {
     pub rx: Receiver<Frame>,
 }
 
-impl PayloadProvider<BoxedPixelData> for PixelBufferSource {
+impl PayloadProvider<BoxedPixelData> for GlSource {
     fn get_payload(&self) -> BoxedPixelData {
         let (data, width, height) = self.rx.recv().unwrap();
         SimplePixelData::new_boxed(width as i32, height as i32, data)
     }
 }
 
-fn get_png_bytes(path: &str) -> anyhow::Result<RgbaImage>{
+fn get_png_bytes(path: &str) -> anyhow::Result<RgbaImage> {
     let path = std::path::Path::new(path);
     Ok(image::open(path)?.to_rgba8())
 }
 
-pub fn get_images(tx: Sender<Frame>, texture: Arc<SendableTexture<BoxedPixelData>>) -> anyhow::Result<()> {
+pub fn get_images(
+    tx: Sender<Frame>,
+    texture: Arc<SendableTexture<BoxedPixelData>>,
+) -> anyhow::Result<()> {
     info!("Starting image stream");
 
-    loop{
+    loop {
         let img1 = get_png_bytes("./assets/1.png").expect("Failed to load image");
         let frame1 = Arc::new((img1.to_vec(), img1.width(), img1.height()));
-        tx.send(Arc::try_unwrap(frame1).unwrap()).expect("Failed to send frame");
+        tx.send(Arc::try_unwrap(frame1).unwrap())
+            .expect("Failed to send frame");
         texture.mark_frame_available();
         info!("Sent frame 1");
         thread::sleep(std::time::Duration::from_secs(1));
         let img2 = get_png_bytes("./assets/2.jpg").expect("Failed to load image");
         let frame2 = Arc::new((img2.to_vec(), img2.width(), img2.height()));
-        tx.send(Arc::try_unwrap(frame2).unwrap()).expect("Failed to send frame");
+        tx.send(Arc::try_unwrap(frame2).unwrap())
+            .expect("Failed to send frame");
         info!("Sent frame 2");
         texture.mark_frame_available();
         thread::sleep(std::time::Duration::from_secs(1));
     }
     Ok(())
 }
-
