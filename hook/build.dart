4a615a1892a2b7a7c5b37b211b474b178d004c78
Mortie <38497452+omortie@users.noreply.github.com>
2026-03-23 05:44:21 +0200

    just bring back env vars for build script

--- hook/build.dart
+++ hook/build.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
-void main(List<String> args) async {
-  
+void main(List<String> args) async {  
+  final envVars = Platform.environment;
+
   await build(args, (input, output) async {
     await RustBuilder(
       assetName: 'rust/frb_generated.io.dart',
       cratePath: 'rust',
+      extraCargoEnvironmentVariables: envVars,
     ).run(
       input: input,
       output: output,
