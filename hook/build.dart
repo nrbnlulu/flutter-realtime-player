b123e460f6a6723bfaa0d698586ccdf03f37634b
Mortie <38497452+omortie@users.noreply.github.com>
2026-03-24 12:39:47 +0200

    added pkg config env vars

--- hook/build.dart
+++ hook/build.dart
@@ -4,18 +4,20 @@ import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 import 'env_utilizer.dart';
 
 const _prebuiltStreamerRootEnvVar = 'GSTREAMER_ROOT_ANDROID';
+const _pkgConfigSysrootEnvVar = 'PKG_CONFIG_SYSROOT_DIR';
 
 void main(List<String> args) async {  
-  final envVars = Env.instance;
+  final envFile = Env.instance;
 
   await build(args, (input, output) async {
     await RustBuilder(
       assetName: 'flutter_realtime_player',
       cratePath: 'rust',
       extraCargoEnvironmentVariables: {
-        _prebuiltStreamerRootEnvVar: envVars.getString(
+        _prebuiltStreamerRootEnvVar: envFile.getString(
           _prebuiltStreamerRootEnvVar,
         ),
+        _pkgConfigSysrootEnvVar: envFile.getString(_prebuiltStreamerRootEnvVar),
       },
     ).run(
       input: input,
