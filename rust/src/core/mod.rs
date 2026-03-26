63e1c72c01bb73a5305d94e605db6efcbde9a43d
Mortie <38497452+omortie@users.noreply.github.com>
2026-03-26 10:17:43 +0200

    Revert "fix logger issue"
    This reverts commit 4c7bd21fca9a50d88a9972abfb00f7a3b33743fa.

--- rust/src/core/mod.rs
+++ rust/src/core/mod.rs
@@ -15,21 +15,14 @@ lazy_static::lazy_static! {
 }
 
 pub(crate) fn init_logger() {
-    let mut is_initialized = IS_INITIALIZED.lock().unwrap();
+    let is_initialized = IS_INITIALIZED.lock().unwrap();
     if *is_initialized {
         return;
     }
-
-    flutter_rust_bridge::setup_default_user_utils();
-
-    // Use a writable directory for logs on Android
-    #[cfg(target_os = "android")] {
-        // since we need to know the package name in android in order to get the writable
-        // logging path which would be let log_dir = "/data/data/com.example.flutter_realtime_player_example/files/logs";
-        // so we just don't enable logging on Android for now, we can get that path from Dart side later.
-        *is_initialized = true;
-        return;
-    } 
+    // only enable logs in non-Android targets
+    // since we need to know the package name in android in order to get the writable
+    // logging path which would be let log_dir = "/data/data/com.example.flutter_realtime_player_example/files/logs";
+    // so we just don't enable logging on Android for now, we can get that path from Dart side later.
     #[cfg(not(target_os = "android"))] {
         let log_dir = "./logs";
         // Try to create the log directory if it doesn't exist
@@ -63,8 +56,8 @@ pub(crate) fn init_logger() {
 
         // leak the guard to keep the file writer alive
         WORKER_GUARD.lock().unwrap().replace(guard);
-        // Default utilities - feel free to custom
-        *is_initialized = true;
-        debug!("Done initializing");
     }
+    // Default utilities - feel free to custom
+    flutter_rust_bridge::setup_default_user_utils();
+    debug!("Done initializing");
 }
